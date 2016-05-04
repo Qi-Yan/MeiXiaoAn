4a368ccb5b524734c4df78190443f888b64b2549
Qi-Yan <18610552481@163.com>
2016-05-04 11:47:25 +0800

    第二次更改

--- src/com/example/meixiaoan/MainActivity.java
+++ src/com/example/meixiaoan/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.meixiaoan;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	private TextView tv ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
